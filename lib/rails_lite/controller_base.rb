bb7ce5e15cb3efcc7701356b9f35345ff75005be
= <=>
2013-11-19 13:17:09 -0500

    MyController renders views

--- lib/rails_lite/controller_base.rb
+++ lib/rails_lite/controller_base.rb
@@ -1,11 +1,14 @@
 require 'erb'
 require_relative 'params'
 require_relative 'session'
+require 'active_support/core_ext'
 
 class ControllerBase
   attr_reader :params
 
-  def initialize(req, res, route_params)
+  def initialize(req, res, route_params = '/')
+    @request = req
+    @response = res
   end
 
   def session
@@ -15,12 +18,25 @@ class ControllerBase
   end
 
   def redirect_to(url)
+    @response.status = 302
+    @response.header['Location'] = url
+    @already_built_response = true
   end
 
-  def render_content(content, type)
+  def render_content(content, type="text/html")
+    @response.body = content
+    @response.content_type = type
+    @already_built_response = true
   end
 
   def render(template_name)
+    controller_name = (self.class.to_s.underscore)
+    path = "views/#{controller_name}/#{template_name}.html.erb"
+
+    contents =  File.read(path)
+
+    body = ERB.new(contents).result(binding)
+    render_content(body)
   end
 
   def invoke_action(name)

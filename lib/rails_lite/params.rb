6f70b89584e0b766fbf3a6c6cfe9683674f33215
= <=>
2013-11-20 14:01:02 -0500

    params takes arrays

--- lib/rails_lite/params.rb
+++ lib/rails_lite/params.rb
@@ -19,16 +19,31 @@ class Params
     @params.to_json.to_s
   end
 
-  private
+
   def parse_www_encoded_form(www_encoded_form)
     query_pairs = URI.decode_www_form(www_encoded_form)
-
+    p query_pairs
     new_params = {}
-    query_pairs.each{|arr| new_params[arr[0]] = arr[1]}
+      query_pairs.each do |key, value|
+      keys_array = parse_key(key) #["cat", "name"]
+
+      current_hash = new_params
+
+      keys_array.each_with_index do |hash_key, index|
+        if( (index+1) == keys_array.count)
+          current_hash[hash_key] = value
+        else
+          current_hash[hash_key] ||= {}
+          current_hash = current_hash[hash_key]
+        end
+      end
+    end
     new_params
   end
 
+  #key = "cat[name]"
   def parse_key(key)
-    @params.keys
+    /(\w*)(?:\[(\w*)(?:\])|)/.match(key)[1,2].compact
   end
+  #returns ["cat", "name"]
 end

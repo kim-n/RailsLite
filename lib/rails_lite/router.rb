6cdee7d9e117158f644ac3a4068641baef9d9da0
= <=>
2013-11-20 14:47:29 -0500

    adds route class

--- lib/rails_lite/router.rb
+++ lib/rails_lite/router.rb
@@ -2,22 +2,37 @@ class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    #controller_class   http_method   pattern        action_name
+    #   user              PUT           /users/:id      users#update
+    #   user             :put           /users/:id      users#update
+
+    @pattern = patterm
+    @http_method = http_method
+    @controller_class = controller_class
+    @action_name = action_name
   end
 
   def matches?(req)
+    http_method == req.request_method.downcase.to_sym
   end
 
   def run(req, res)
+     ControllerBase.new(req,res).invoke_action(@action_name)
   end
 end
 
 class Router
+  # given an HTTPRequest, figure out which Route matches the requested URL.
+  # Once found, instantiate the Route's controller, and run the appropriate method.
+
+
   attr_reader :routes
 
   def initialize
   end
 
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
   def draw(&proc)
@@ -25,6 +40,7 @@ class Router
 
   [:get, :post, :put, :delete].each do |http_method|
     # add these helpers in a loop here
+    end
   end
 
   def match(req)
